4dc0ba8d55773aef31f888cb84ca4ecc2f9b7431
William Loosman <william.wl@live.nl>
2014-09-11 11:10:19 +0200

    blog example

--- views/blog/javascript/desktop.js
+++ views/blog/javascript/desktop.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 	
-	if($(window).height() > $('#mainColumn').height()){
-		$('#mainColumn').css('min-height', ($(window).height() - $('header.main').height() - $('footer.main').height()) + 'px');
+	var windowHeight = $(window).height();
+	
+	if(windowHeight > $('#mainColumn').height()){
+		$('#mainColumn').css('min-height', (windowHeight - $('header.main').height() - $('footer.main').height()) + 'px');
+		var old = window.scrollY;
+		/*window.setInterval(function(){
+			window.scrollTo(0,0);
+			window.scrollTo(0,old);
+		}, 500);*/
 	}
 	
 });
